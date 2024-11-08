142e841d4f36be75ffbca11bdd895ab3ed525b99
Kwame <136204589+addo12kwame@users.noreply.github.com>
2024-11-08 02:28:47 -0500

    added the producer

--- youtube.py
+++ youtube.py
@@ -3,9 +3,23 @@ import logging
 import sys
 
 import requests
-
+from confluent_kafka import SerializingProducer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import StringSerializer
 
 from configs import config
+
+
+def fetch_video_page(google_api_key,video_id,page_token=None):
+    response = requests.get('https://www.googleapis.com/youtube/v3/videos', params={"key": google_api_key,
+                                                                                       'id': video_id,
+                                                                                       "part": "snippet,statistics",
+                                                                                       "pageToken":page_token})
+    payload = json.loads(response.text)
+    logging.debug("Got %s",payload)
+    return payload
+
 
 def fetch_playlist_items_page(google_api_key,youtube_playlist_id,page_token=None):
     response = requests.get('https://www.googleapis.com/youtube/v3/playlistItems', params={"key": google_api_key,
@@ -13,7 +27,7 @@ def fetch_playlist_items_page(google_api_key,youtube_playlist_id,page_token=None
                                                                                        "part": "contentDetails",
                                                                                        "pageToken":page_token})
     payload = json.loads(response.text)
-    logging.debug("Got %s",response.text)
+    logging.info("Got %s",payload)
     return payload
 
 def fetch_playlist_items(google_api_key, youtube_playlist_id, page_token= None):
@@ -27,18 +41,63 @@ def fetch_playlist_items(google_api_key, youtube_playlist_id, page_token= None):
     if next_page_token is not None:
         yield from fetch_playlist_items(google_api_key, youtube_playlist_id, next_page_token)
 
+def fetch_videos(google_api_key, youtube_playlist_id, page_token= None):
+    payload = fetch_video_page(google_api_key, youtube_playlist_id,page_token)
+
+    yield from payload['items']
+
+    next_page_token = payload.get('nextPageToken')
+
+
+    if next_page_token is not None:
+        yield from fetch_playlist_items(google_api_key, youtube_playlist_id, next_page_token)
+
+def summarize_video(video):
+    return {
+        "video_id": video["id"],
+        "title": video["snippet"]["title"],
+        "views": int(video["statistics"].get("viewCount",0)),
+        "likes": int(video["statistics"].get("likeCount",0)),
+        "comments": int(video["statistics"].get("commentCount",0)),
+    }
+
+def on_delivery(err,record):
+    pass
+
 
 def main():
     logging.info('START')
+    schema_registry_client = SchemaRegistryClient(config['schema_registry'])
+    youtube_videos_value_schema  = schema_registry_client.get_latest_version("youtube_videos-value")
+
+    kafka_config = config['kafka'] | {
+        "key.serializer": StringSerializer(),
+        'value.serializer': AvroSerializer(schema_registry_client=schema_registry_client, schema_str=youtube_videos_value_schema.schema.schema_str)
+
+    }
+    producer = SerializingProducer(kafka_config)
 
     google_api_key = config['google_api_key']
     youtube_playlist_id = config["youtube_playlist_id"]
 
     for video_item in fetch_playlist_items(google_api_key,youtube_playlist_id):
-        logging.info('Got %s', video_item)
+        video_id = video_item["contentDetails"]["videoId"]
+        for video in fetch_videos(google_api_key,video_id):
+            logging.info('Got %s', summarize_video(video))
 
+            producer.produce(
+                topic="youtube_videos",
+                key=video_id,
+                value={
+                    "TITLE": video["snippet"]["title"],
+                    "VIEWS": int(video["statistics"].get("viewCount",0)),
+                    "LIKES": int(video["statistics"].get("likeCount",0)),
+                    "COMMENTS": int(video["statistics"].get("commentCount",0)),
+                },
+                on_delivery=on_delivery
+            )
 
-
+    producer.flush()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
